920e587925e24bf3abe3fed1b5a58f2093a290ca
Adrian Montemayor Rojas <adrianmontemayorrojas@MacBook-Air-de-Adrian.local>
2025-03-04 18:42:14 -0600

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+const piece = {
+  type: "peon",
+  position: "a2",
+  movementType: "1f 2f 1d"
+}
+
+function App() {
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
